753173b47bb92a62396ee52f87f3de7f03116629
Ariel <apb0036.alu.medac.es>
2026-04-30 11:00:53 +0200

    Arreglos y aumento de atributos

--- src/main/java/com/example/libros.java
+++ src/main/java/com/example/libros.java
@@ -1,5 +1,91 @@
 package com.example;
 
 public enum libros {
+    LIBRO1(101,"El Quijote", "Miguel de Cervantes", 863, "Ficción", true, 10),
+    LIBRO2(102,"Cien Años de Soledad", "Gabriel García Márquez", 417, "Ficción", true, 8),
+    LIBRO3(103,"Metamorfosis", "Franz Kafka", 201, "Ficción", true, 5),
+    LIBRO4(104,"La Divina Comedia", "Dante Alighieri", 798, "Epic poetry", true, 3),
+    LIBRO5(105,"La ciudad y los perros", "Mario Vargas Llosa", 416, "Ficción", true, 7);
+    
+    private int codigo;
+    private String titulo;
+    private String autor;
+    private int paginas;
+    private String genero;
 
+    private boolean disponibleParaPrestamo;
+    private int stock;
+
+    libros(int codigo, String titulo, String autor, int paginas, String genero, boolean disponibleParaPrestamo, int stock) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+        this.genero = genero;
+        this.disponibleParaPrestamo = disponibleParaPrestamo;
+        this.stock = stock;
+    }
+
+
+    //identificador único para cada libro
+    public int codigo() {
+        return codigo;
+    }
+
+    //datos uunicos de cada libro
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+
+    //Sistema de préstamo e información de stock
+
+    public boolean isDisponibleParaPrestamo() {
+        return disponibleParaPrestamo;
+    }
+
+    public void setDisponibleParaPrestamo(boolean disponibleParaPrestamo) {
+        this.disponibleParaPrestamo = disponibleParaPrestamo;
+        if (disponibleParaPrestamo) {
+            stock--; // Si no está disponible para préstamo, el stock se reduce en 1
+        } else {
+            stock++; // Si se devuelve el libro, el stock se incrementa en 1
+        }
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
---
+++ src/main/java/com/example/main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+  public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println(libros.LIBRO1.getTitulo()+" ");
+        System.out.print(libros.LIBRO1.getAutor()+" ");
+        System.out.print(libros.LIBRO1.getPaginas()+" ");
+        System.out.print(libros.LIBRO1.getGenero()+" ");
+        System.out.print(libros.LIBRO1.isDisponibleParaPrestamo()+" ");
+        System.out.print(libros.LIBRO1.getStock()+" ");
+    }
+  }
